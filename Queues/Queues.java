631fcfc904847a2b08fb0321f464011fb4dfd057
joannamcd <joamcd@gmail.com>
2023-11-22 18:57:28 -0600

    trees.txt

--- Queues/Queues.java
+++ Queues/Queues.java
@@ -63,13 +63,13 @@ class Queue{
 public class Queues{
     public static void main(String[] args){
         Queue queue = new Queue(1000);
-
+        System.out.println("*************\n ARRAY IMPLEMENTATION\n ***************");
         queue.enqueue(10);
         queue.enqueue(20);
         queue.enqueue(30);
         queue.enqueue(40);
 
-        System.out.println("ARRAY IMPLEMENTATION");
+        
         System.out.println(queue.dequeue() + " dequeued from queue\n");
         System.out.println("Front item is " + queue.front());
         System.out.println("Rear item is " + queue.rear());
